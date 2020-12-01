345ee2e9bd29103164341aa8b3e2d81f04fbfe3a
Milan Murray <x00162027@mytudublin.ie>
2020-12-01 23:07:45 +0000

    Fixed js overwriting margins

--- sources/scripts.js
+++ sources/scripts.js
@@ -4,7 +4,7 @@ function changeToLightMode() {
     console.log("Dark mode: " + darkMode);
     if (darkMode == true) {
         document.getElementById("nav").className = "navbar navbar-expand-lg bg-light navbar-light";
-        document.getElementById("body").className = "card bg-light";
+        document.getElementById("body").className = "card bg-light my-2";
         document.getElementsByClassName("text-light text-center")[0].className = "text-dark text-center";
         document.getElementsByClassName("text-light text-center")[0].className = "text-dark text-center";
         let lightTextFields = document.getElementsByClassName("text-light").length;
@@ -20,7 +20,7 @@ function changeToDarkMode() {
     console.log("Dark mode: " + darkMode);
     if (darkMode == false) {
         document.getElementById("nav").className = "navbar navbar-expand-lg bg-dark navbar-dark";
-        document.getElementById("body").className = "card bg-dark";
+        document.getElementById("body").className = "card bg-dark my-2";
         document.getElementsByClassName("text-dark text-center")[0].className = "text-light text-center";
         document.getElementsByClassName("text-dark text-center")[0].className = "text-light text-center";
         let darkTextFields = document.getElementsByClassName("text-dark").length;
