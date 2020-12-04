11627c5926f79446811314c1d4b45536b47886c3
Milan Murray <x00162027@mytudublin.ie>
2020-12-04 01:40:59 +0000

    Removed unrequired navigation, improved readability of table and added table toggle

--- sources/scripts.js
+++ sources/scripts.js
@@ -1,13 +1,16 @@
 let darkMode = true;
+let tableVisable = false;
 
 function changeToLightMode() {
     console.log("Dark mode: " + darkMode);
     if (darkMode == true) {
-        document.getElementById("nav").className = "navbar navbar-expand-lg bg-light navbar-light";
-        document.getElementById("body").className = "card bg-light my-2";
+        // document.getElementById("nav").className = "navbar navbar-expand-lg bg-light navbar-light";
+        document.getElementById("body").className = "container card bg-light my-3";
         document.getElementsByClassName("text-light text-center")[0].className = "text-dark text-center";
         document.getElementsByClassName("text-light text-center")[0].className = "text-dark text-center";
-        document.getElementById("scroll_to_top_btn").className = "btn btn-dark btn-sm ml-auto m-3 fixed-bottom";
+        // document.getElementById("scroll_to_top_btn").className = "btn btn-dark btn-sm ml-auto m-3 fixed-bottom";
+        document.getElementsByTagName("table")[0].className = "table table-striped table-bordered";
+        document.getElementsByTagName("button")[2].className = "btn btn-dark btn-sm float-right";
         let lightTextFields = document.getElementsByClassName("text-light").length;
         for (i = 0; i < lightTextFields; i++) {
             document.getElementsByClassName("text-light")[0].className = "text-dark";
@@ -20,11 +23,13 @@ function changeToLightMode() {
 function changeToDarkMode() {
     console.log("Dark mode: " + darkMode);
     if (darkMode == false) {
-        document.getElementById("nav").className = "navbar navbar-expand-lg bg-dark navbar-dark";
-        document.getElementById("body").className = "card bg-dark my-2";
+        // document.getElementById("nav").className = "navbar navbar-expand-lg bg-dark navbar-dark";
+        document.getElementById("body").className = "container card bg-dark my-3";
         document.getElementsByClassName("text-dark text-center")[0].className = "text-light text-center";
         document.getElementsByClassName("text-dark text-center")[0].className = "text-light text-center";
-        document.getElementById("scroll_to_top_btn").className = "btn btn-light btn-sm ml-auto m-3 fixed-bottom";
+        // document.getElementById("scroll_to_top_btn").className = "btn btn-light btn-sm ml-auto m-3 fixed-bottom";
+        document.getElementsByTagName("table")[0].className = "table table-dark table-striped table-bordered";
+        document.getElementsByTagName("button")[2].className = "btn btn-light btn-sm float-right";
         let darkTextFields = document.getElementsByClassName("text-dark").length;
         for (i = 0; i < darkTextFields; i++) {
             document.getElementsByClassName("text-dark")[0].className = "text-light";
@@ -34,6 +39,17 @@ function changeToDarkMode() {
     }
 }
 
+function toggleTableDisplay()  {
+    if (tableVisable == false) {
+      document.getElementsByTagName("table")[0].style.display = "";
+      tableVisable = true;
+    } else {
+      document.getElementsByTagName("table")[0].style.display = "none";
+      tableVisable = false;
+    }
+}
+
+/*
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -48,3 +64,4 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+*/
